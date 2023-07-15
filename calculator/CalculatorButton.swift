a873041927e2ec8da3a108e23950fa5d4ffc10c6
Sean Davie <sean.davie.m@gmail.com>
2023-07-15 19:32:10 -0400

    practice(UIButtons): messing with UIButton class
    - create UIButton class
    - mess around with autolayout vs. frame
    - size button based on parent, but within viewcontroller (can this somehow be defined within the class?)

---
+++ calculator/CalculatorButton.swift
@@ -0,0 +1,46 @@
+//
+//  CalculatorButton.swift
+//  calculator
+//
+//  Created by Sean Davie on 7/14/23.
+//
+
+import UIKit
+
+class CalculatorButton: UIButton {
+    
+    var buttonColor: UIColor?
+    var label: String?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupButton()
+    }
+    
+    init(buttonColor: UIColor, label: String) {
+        self.buttonColor = buttonColor
+        self.label = label
+        super.init(frame: .zero)
+
+        setupButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButton()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = (frame.height) / 2
+    }
+    
+    func setupButton() {
+        backgroundColor = buttonColor
+        clipsToBounds = true
+        setTitle(label, for: .normal)
+        setTitleColor(.white, for: .normal)
+    }
+
+}
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var testButton = CalculatorButton(buttonColor: .orange, label: "1")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupView()
+    }
+    
+    func setupView() {
+        view.addSubview(testButton)
+        addConstraints()
+    }
+    
+    func addConstraints() {
+        testButton.translatesAutoresizingMaskIntoConstraints = false
+        testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        testButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25, constant: -30).isActive = true
+        testButton.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25, constant: -30).isActive = true
     }
 
 
